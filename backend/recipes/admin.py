8384808be0f2ca66fbf99c968641ccf783fc39ef
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-11 17:02:00 +0300

    Add new Admin classes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from recipes.models import Recipe, Ingredient, Tag
 
-# Register your models here.
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+
+@admin.register(Recipe)
+class AdminRecipe(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+    )
+    inlines = (TagInline,)
+    list_filter = ('author', 'name', 'tag')
+    search_fields = ('author', 'name', 'tag')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Ingredient)
+class AdminIngredient(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    list_filter = ('name',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
