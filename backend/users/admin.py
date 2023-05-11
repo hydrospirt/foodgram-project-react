9085a093d755866d2faab0cc26970e37ec6e4596
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-11 20:08:52 +0300

    Change to UserAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from users.models import CustomUser
 
 
 @admin.register(CustomUser)
-class AdminUser(admin.ModelAdmin):
+class AdminUser(UserAdmin):
     list_display = (
         'pk',
         'username',
         'email',
         'first_name',
         'last_name',
-        'password',
         'is_active',
         'last_login',
         'is_staff',
+        'date_joined',
     )
     list_filter = ('email', 'first_name')
     search_fields = ('email', 'first_name')
     list_display_links = ('pk', 'username')
+    readonly_fields = ('date_joined',)
     empty_value_display = '-пусто-'
