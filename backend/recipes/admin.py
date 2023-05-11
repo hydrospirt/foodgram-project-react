20340f1c633c79ba84c87a02d35c25aedd626697
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-11 19:20:37 +0300

    Add new classes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from recipes.models import Recipe, Ingredient, Tag
+from recipes.models import Recipe, Ingredient, Tag, Favorites, Subscriptions, ShoppingCart
 
 @admin.register(Recipe)
 class AdminRecipe(admin.ModelAdmin):
@@ -9,7 +9,12 @@ class AdminRecipe(admin.ModelAdmin):
     )
     list_filter = ('author', 'name', 'tag')
     search_fields = ('author', 'name', 'tag')
+    readonly_fields = ('amount_favorites',)
     empty_value_display = '-пусто-'
+
+    @admin.display(description='Кол-во добавлений в избранное')
+    def amount_favorites(self, obj):
+        return obj.favorites.count()
 
 
 @admin.register(Ingredient)
@@ -20,4 +25,49 @@ class AdminIngredient(admin.ModelAdmin):
     )
     list_filter = ('name',)
     search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Tag)
+class AdminTag(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'color',
+        'slug'
+    )
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Favorites)
+class AdminFavorites(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'recipe',
+    )
+    list_filter = ('user', 'recipe')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subscriptions)
+class AdminSubscriptions(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'author',
+    )
+    list_filter = ('user', 'author')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(ShoppingCart)
+class AdminShoppingCart(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'recipe',
+    )
+    list_filter = ('user', 'recipe')
     empty_value_display = '-пусто-'
