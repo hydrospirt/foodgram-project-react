2d6216f3c7c12e19911e8d1829cd7ed0c81065c8
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-12 15:08:50 +0300

    Add CustomUser,Tag ViewSets

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.serializers import CustomUserSerializer, TagSerializer
+from recipes.models import Recipe, Tag, Ingredient
+from users.models import CustomUser
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
