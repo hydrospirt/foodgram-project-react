3ff53401955c9a6be172dbdde9950bd898b3a0f6
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-12 15:34:01 +0300

    Add Recipe and Ingredient Serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -19,3 +19,15 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
