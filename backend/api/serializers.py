bdd317c9b682b3395385089122aceaf694c40d1d
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-12 13:31:40 +0300

    Add User and Tag serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,16 @@
 from rest_framework import serializers
 
 from recipes.models import Recipe, Tag, Ingredient
+from users.models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+            )
