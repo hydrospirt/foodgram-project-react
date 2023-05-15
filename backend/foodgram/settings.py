6b209986b1e48e79f62686e9ca65f4407c256591
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-15 15:57:27 +0300

    Add pagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -142,5 +142,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # REST Framework
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',)
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
