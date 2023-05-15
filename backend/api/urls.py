904d0b246872aa84d8f9898d0e047d3d6b319c3a
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-15 21:19:40 +0300

    Change CustomUser to User

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import CustomUserViewSet, TagViewSet, RecipeViewSet, IngredientViewSet
+from api.views import UserViewSet, TagViewSet, RecipeViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register(r'tag', TagViewSet, basename='tag')
-router.register(r'users', CustomUserViewSet, basename='users')
+router.register(r'users', UserViewSet, basename='users')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
