df46ee495c66cbc7d9c0deae8a45ceaf7941c0b9
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-16 12:59:36 +0300

    Fix path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,6 +11,6 @@ router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls')),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
