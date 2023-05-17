3485a70f778bf2be1f78222cab5866de8660fd80
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-17 13:10:50 +0300

    Add favorite

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,8 +5,8 @@ from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
 from djoser.views import UserViewSet
 from rest_framework import viewsets, permissions, filters, status
-from api.serializers import UserSerializer, TagSerializer, RecipeSerializer, IngredientSerializer, UserSubSerializer
-from recipes.models import Recipe, Tag, Ingredient, Subscriptions
+from api.serializers import UserSerializer, TagSerializer, RecipeSerializer, IngredientSerializer, UserSubSerializer, ShortRecipeSerializer
+from recipes.models import Recipe, Tag, Ingredient, Subscriptions, Favorites
 from api.permissions import IsAuthorOrAdminOrReadOnly, IsAdminOrReadOnly
 
 User = get_user_model()
@@ -38,7 +38,6 @@ class UserViewSet(UserViewSet, viewsets.ModelViewSet):
     @permission_classes([permissions.IsAuthenticated])
     def subscribe(self, request, *args, **kwargs):
         pk = kwargs.get('id',)
-        print(pk)
         author = get_object_or_404(User, pk=pk)
         user = request.user
         if request.method == 'POST':
@@ -89,6 +88,35 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = (IsAuthorOrAdminOrReadOnly,)
+
+    @action(methods=('POST', 'DELETE'),
+            detail=True,
+            permission_classes=(permissions.IsAuthenticated,))
+    def favorite(self, request, *args, **kwargs):
+        pk = kwargs.get('id',)
+        recipe = get_object_or_404(Recipe, pk=pk)
+        user = request.user
+        if request.method == 'POST':
+            if Favorites.objects.filter(user=user, recipe=recipe).exists():
+                return Response(
+                    {'error': 'Рецепт уже добавлен в избранное'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            fav = Favorites(user=user, recipe=recipe)
+            fav.save()
+            serializer = ShortRecipeSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        elif request.method == 'DELETE':
+            try:
+                Favorites.objects.get(user=user, recipe=recipe).delete()
+            except Favorites.DoesNotExist:
+                return Response(
+                    {'errors': f'Рецепта нет в избранном'},
+                    status=status.HTTP_400_BAD_REQUEST
+                    )
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
