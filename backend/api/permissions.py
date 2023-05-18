437860de607de78062ec52872c05ca1675200b54
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-18 16:55:05 +0300

    flake8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework. permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsAdminOrReadOnly(BasePermission):
@@ -14,13 +14,13 @@ class IsAdminOrReadOnly(BasePermission):
 
 class IsAuthorOrAdminOrReadOnly(BasePermission):
 
-     message = 'Вы не можете изменить чужой контент'
+    message = 'Вы не можете изменить чужой контент'
 
-     def has_permission(self, request, view):
+    def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
                 or request.user.is_authenticated)
 
-     def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
-                or (obj.author == request.user or
-                    request.user.is_superuser))
+                or (obj.author == request.user
+                    or request.user.is_superuser))
