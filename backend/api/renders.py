86f66441f32f6924a1fe97b649ed5bff47189159
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-18 16:34:03 +0300

    Add IngredientDataRendererTXT

---
+++ backend/api/renders.py
@@ -0,0 +1,18 @@
+import io
+from rest_framework import renderers
+
+INGREDIENT_DATA_FILE_HEADERS = ('Название', 'Кол-во', 'Ед. измерения')
+
+class IngredientDataRendererTXT(renderers.BaseRenderer):
+    media_type = 'text/plain'
+    format = 'txt'
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        text_buffer = io.StringIO()
+        text_buffer.write(' '.join(header for header in INGREDIENT_DATA_FILE_HEADERS) + '\n')
+
+        for ingredient_data in data:
+            text_buffer.write('    '.join(str(i) for i in list(ingredient_data.values())) + '\n')
+
+        return text_buffer.getvalue()
+
