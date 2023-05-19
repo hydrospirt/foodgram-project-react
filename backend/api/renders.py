956f2566131202fc6f208a2cb07256b8306fd35c
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-19 19:27:46 +0300

    Add title and footer

--- backend/api/renders.py
+++ backend/api/renders.py
@@ -1,5 +1,4 @@
 import io
-
 from rest_framework import renderers
 
 INGREDIENT_DATA_FILE_HEADERS = ('Название', 'Кол-во', 'Ед. измерения')
@@ -12,11 +11,25 @@ class IngredientDataRendererTXT(renderers.BaseRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         text_buffer = io.StringIO()
         text_buffer.write(
+            '''
+_________________________________________________________________
+|    _____                    _                                 |
+|   |  ___|  ___    ___    __| |  __ _  _ __   __ _  _ __ ___   |
+|   | |_    / _ \  / _ \  / _` | / _` || '__| / _` || '_ ` _  | |
+|   |  _|  | (_) || (_) || (_| || (_| || |   | (_| || | | | | | |
+|   |_|     \___/  \___/  \__,_| \__, ||_|    \__,_||_| |_| |_| |
+|                                |___/                          |
+|_______________________________________________________________|
+'''
+        )
+        text_buffer.write(
             ' '.join(header for header in INGREDIENT_DATA_FILE_HEADERS) + '\n')
 
         for ingredient_data in data:
             text_buffer.write(
-                '    '.join(str(i) for i in list(ingredient_data.values()))
+                ' '.join(str(i) for i in list(ingredient_data.values()))
                 + '\n')
-
+        text_buffer.write('_' * 65 + '\n')
+        text_buffer.write(f'{"Создано в приложении ©": ^65}' + '\n')
+        text_buffer.write('_' * 65 + '\n')
         return text_buffer.getvalue()
