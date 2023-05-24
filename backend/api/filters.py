69eb5fb436994585cf344b40440b1cc3d5f22656
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-24 16:29:33 +0300

    Add Recipe filter

---
+++ backend/api/filters.py
@@ -0,0 +1,11 @@
+from django_filters import FilterSet
+from recipes.models import Recipe
+
+class RecipeFilter(FilterSet):
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'author',
+            'tags__slug',
+        )
