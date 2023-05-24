d7d6f4eae1ff53159138181f2e8bd843e061d878
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-24 13:34:20 +0300

    Add default page size and max for limit

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -3,3 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class LimitPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'limit'
+    page_size = 20
+    max_page_size = 200
