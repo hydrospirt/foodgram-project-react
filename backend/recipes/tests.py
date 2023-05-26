7473aa150ba41c1e863564353a9ddb1e701afe40
Эдуард Гумен <hydrospirt@yandex.ru>
2023-05-26 20:53:56 +0300

    Fix isort

--- backend/recipes/tests.py
+++ backend/recipes/tests.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
 from recipes.models import (Favorites, Ingredient, IngredientAmount, Recipe,
                             ShoppingCart, Subscriptions, Tag)
 
