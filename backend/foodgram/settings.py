a6eb710641989f4e0ea7a95ebfdf1ec43afab943
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-01 16:40:25 +0300

    Add new value PAGE_SIZE

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -152,7 +152,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
