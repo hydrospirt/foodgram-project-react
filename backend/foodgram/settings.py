b3df29654f739c4ccd32c5aaf75050196573ae8a
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-02 18:44:45 +0300

    Fix ALLOWED_HOSTS

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(",")
+ALLOWED_HOSTS = [] if not any(ALLOWED_HOSTS) else ALLOWED_HOSTS
 
 
 # Application definition
