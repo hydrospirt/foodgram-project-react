e6323738a0571e96b7ec4dcd5bdb363dca286b6c
Эдуард Гумен <hydrospirt@yandex.ru>
2023-06-04 18:03:03 +0300

    Fix UserSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -63,17 +63,6 @@ class UserSerializer(serializers.ModelSerializer):
         return Subscriptions.objects.filter(author=user_id,
                                             user=request_user).exists()
 
-    # def create(self, validated_data):
-    #     user = User(
-    #         email=validated_data['email'],
-    #         username=validated_data['username'],
-    #         first_name=validated_data['first_name'],
-    #         last_name=validated_data['last_name'],
-    #     )
-    #     user.set_password(validated_data['password'])
-    #     user.save()
-    #     return user
-
 
 class ShortRecipeSerializer(serializers.ModelSerializer):
     class Meta:
